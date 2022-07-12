910ad4a2cb03bbdc0790076e6f78e639ebb36f33
nk321626 <nk321626@gmail.com>
2022-07-12 21:17:50 +0530

    Edited: Emp Wage Program

--- EmpWageComp/EmpAttendance.cs
+++ EmpWageComp/EmpAttendance.cs
@@ -18,13 +18,8 @@ namespace EmpWageComp
             if (randomInt == EMP_PRESENT)
             {
                 Console.WriteLine("Employee is Present");
-                
-                int empHrs = 0;
-                int empWage_Per_hr = 20;
-                int daily_Emp_Wage = 0;
-                empHrs = 8;
-                daily_Emp_Wage = empHrs * empWage_Per_hr;
-                Console.WriteLine("Daily Employee Wage is : " + daily_Emp_Wage);
+                EmpWage.GetEmpWage();
+                Console.ReadLine();
             }
             else
             {
---
+++ EmpWageComp/EmpWage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageComp
+{
+    public class EmpWage
+    {
+
+        public static void GetEmpWage()
+        {
+            int empHrs = 0;
+            int empWage_Per_hr = 20;
+            int daily_Emp_Wage = 0;
+            empHrs = 8;
+            daily_Emp_Wage = empHrs * empWage_Per_hr;
+            Console.WriteLine("Daily Employee Wage is : " + daily_Emp_Wage);
+        }
+    }
+}
