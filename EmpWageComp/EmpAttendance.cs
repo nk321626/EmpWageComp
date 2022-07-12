5ec4d9f753ddc6a1b57d688946f3334b07e67d0b
nk321626 <nk321626@gmail.com>
2022-07-12 21:02:26 +0530

    Added: Emp Wage Program

--- EmpWageComp/EmpAttendance.cs
+++ EmpWageComp/EmpAttendance.cs
@@ -18,6 +18,13 @@ namespace EmpWageComp
             if (randomInt == EMP_PRESENT)
             {
                 Console.WriteLine("Employee is Present");
+                
+                int empHrs = 0;
+                int empWage_Per_hr = 20;
+                int daily_Emp_Wage = 0;
+                empHrs = 8;
+                daily_Emp_Wage = empHrs * empWage_Per_hr;
+                Console.WriteLine("Daily Employee Wage is : " + daily_Emp_Wage);
             }
             else
             {
